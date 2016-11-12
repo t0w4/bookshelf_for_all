c6b6ac8cba03b4065c5c0859140cd218172ddb6f
tokunagawataru <tokunaga.wataru31@gmail.com>
2016-11-12 14:31:42 +0900

    search_js_repair

--- app/assets/javascripts/cerulean.js
+++ app/assets/javascripts/cerulean.js
@@ -2,7 +2,6 @@
 // Bootswatch
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require cerulean/loader
 //= require cerulean/bootswatch
 //= require cerulean/bootstrap-tagsinput
