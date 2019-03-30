f8d9e43b3308274b2ac6704771117fd369108f90
tokunagawataru <tokunaga.wataru31@gmail.com>
2019-03-30 18:30:30 +0900

    gem update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107144440) do
+ActiveRecord::Schema.define(version: 20190330092846) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title",           limit: 255
