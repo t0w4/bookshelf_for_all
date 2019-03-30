4449ee73c9f0f6f7c3b5a554e328e62f7a02cbc8
tokunagawataru <tokunaga.wataru31@gmail.com>
2019-03-30 22:55:27 +0900

    change bedore_filter to before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_current_user
+  before_action :set_current_user
 
   def set_current_user
     User.current = current_user
